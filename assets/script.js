7513ae704c0d0b93ff486b4bb2f9b04285e4b24a
Sam <Sambose426@yahoo.com>
2023-08-05 00:19:58 -0600

    added a search function that shows the returned data in the console

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+// API Call https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+// API
+var apiKey = "139a03101b47f7ef6ab0275fb6f50464";
+var URL = " https://api.openweathermap.org/data/2.5/weather?q=";
+
+// Refrences
+var mainEl = $("#mainSection");
+var oldLinks = $("#oldLinks");
+var searchEl = $("#SearchBtn");
+var inputEl = $("#input");
+
+function Search(searchWord) {
+	fetch(URL + searchWord + "&appid=" + apiKey)
+		.then((response) => {
+			if (response.ok) return response.json();
+			else console.log("Error: RESPONSE NOT OK");
+		})
+		.then((response) => {
+			console.log(response);
+		});
+}
+
+searchEl.on("click", function () {
+	var searchWord = inputEl.val();
+
+	Search(searchWord);
+});
