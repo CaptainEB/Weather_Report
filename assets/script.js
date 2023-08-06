9f92d51c4099fa5c95d51795da6c8528ad50d2b2
Sam <Sambose426@yahoo.com>
2023-08-06 03:38:35 -0600

    added dayjs inside script.js file and added appended the header of the main section for the searched city

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,16 @@
 
 // API
 var apiKey = "139a03101b47f7ef6ab0275fb6f50464";
-var URL = " https://api.openweathermap.org/data/2.5/weather?q=";
+var URL = " https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
 
 // Refrences
 var mainEl = $("#mainSection");
 var oldLinks = $("#oldLinks");
 var searchEl = $("#SearchBtn");
 var inputEl = $("#input");
+
+// Global Variables
+var today = dayjs().format("D/M/YYYY");
 
 function Search(searchWord) {
 	fetch(URL + searchWord + "&appid=" + apiKey)
@@ -17,11 +20,17 @@ function Search(searchWord) {
 			else console.log("Error: RESPONSE NOT OK");
 		})
 		.then((response) => {
+			// adds the result to the screen
+			var headerEl = $("<p>");
+			headerEl.text(response.name + " " + today + " " + response.weather[0].icon);
+
+			mainEl.append(headerEl);
 			console.log(response);
 			return response;
 		});
 }
 
+// Adds old searches to the screen
 function CreateElement(element) {
 	var button = $("<button>").text(element);
 	button.addClass("oldLink");
